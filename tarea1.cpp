839e52942f7798b763fa22376e28eb7ff24992c5
vcastillo222 <128198534+vcastillo222@users.noreply.github.com>
2025-04-14 17:15:19 -0400

    Create tarea1.cpp

---
+++ tarea1.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std; 
+using std::cerr;
+using std::endl;
+#include <fstream>
+using std::ofstream;
+#include <cstdlib>
+
+
+
+
+class alter { 
+    bool tipo;
+    string respuesta;
+    string alternativa; 
+    
+    public: 
+    
+     
+
+}
+        
+
+class preguntas {alter, tax) public:  
+
+
+string taxonomia;
+
+
+public:
+std::ofstream Preguntas("Preguntas.txt");  
+
+bool esc;
+cout << "desea escribir una pregunta? si=1 no=0" << endl;
+cin >> esc;
+while(esc == true)
+{
+    Preguntas.open; 
+    cout << "escriba la letra de la taxonomia (a, b, c, d, e, f, g, h) en el primer caracter\n Ejemplo: -cuantos"
+    cin >> Preguntas; 
+    cout >> "desea elegir otra opcion del menu">endl;
+    cin << menu;
+}
+
+}  ;
+
+
+class prueba (preguntas) { //(std asignatura, preguntas.txt){ public:  
+string asignatura;
+string pregunta;
+
+public:
+
+std::ofstream Prueba("Prueba.txt")
+
+
+//open preguntas.txt, escribir pregunta 
+
+}  
+
+
+
+int main() 
+{ 
+    
+    prueba prueba1;
+    int menu; 
+    menu=0; 
+    cout << "menu" << endl; 
+    cout << archivo -> asignaturas.txt 
+    cout << " 1 crear item " 
+    cout << " 2 actualizar" << endl; 
+    cout << "3 borrar item " << endl; 
+    cout << "4 buscar por taxonomia" << endl; 
+    cout << "5 salir " << endl; 
+    cout << "escriba el nu´mero que se muestra antes de opcio´n deseada \n (ej: si desea crear un item, escriba 1" << endl; 
+    cin >> menu; 
+    
+    if (menu=1) 
+    { 
+        alter
+        
+    } 
+    else if (menu=2) {  
+preguntas 
+} else if (menu=3) {  
+    preguntas 
+} else if (menu=4) {  
+cout << "ingrese taxonomia" 
+cin >> tax;
+while (tax==P[1])
+
+} else if (menu=5) {  
+ break;
+ default: 
+ cout << "No es una opcio'n va'lida. \n";
+ cin >> menu;
+ break;
+ 
+cin >> choice;
+
+}  
+
+return 0; 
+
+} 
